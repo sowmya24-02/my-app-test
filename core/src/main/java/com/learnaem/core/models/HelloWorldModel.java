33237015e7b4f98ff230db260e8732b121522722
sowmya24-02 <sowmyanerella11@gmail.comgit config --global user.name Your>
2025-02-11 10:34:16 +0530

    verifying eclipse for commit

--- core/src/main/java/com/learnaem/core/models/HelloWorldModel.java
+++ core/src/main/java/com/learnaem/core/models/HelloWorldModel.java
@@ -56,7 +56,7 @@ public class HelloWorldModel {
 
         message = "Hello World!\n"
             + "Resource type is: " + resourceType + "\n"
-            + "Current page is: Hello world" + currentPagePath + "\n";
+            + "Current page is: Hello" + currentPagePath + "\n";
     }
 
     public String getMessage() {
